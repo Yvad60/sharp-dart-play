e9b50949ce7b3815b3ef99b8387b2524c7e5bcfb
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-04-03 22:37:25 +0200

    Add variables

---
+++ variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  String firstName = 'John';
+  String lastName = 'Doe';
+  int age = 50;
+  print("Hello my name is $firstName $lastName, and I am $age years old");
+
+  var nickName =
+      'T dog'; //type inference (variables declared with var can take initial state type when type not specified)
+  print(nickName);
+}
